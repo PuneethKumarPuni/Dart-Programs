8223b23b4c7ed265a570dfe635d5fb6072120afe
PuneethKumarPuni <136142481+PuneethKumarPuni@users.noreply.github.com>
2023-12-20 15:47:19 +0530

    Add files via upload

---
+++ 01 Arithmatic operation of two numbers.dart
@@ -0,0 +1,19 @@
+void main() {
+  var n1 = 10;
+  var n2 = 20;
+
+  var sum = n1 + n2;
+  print("sum of ${n1} and ${n2} is ${sum}");
+
+  var diff = n2 - n1;
+  print("difference of ${n1} and ${n2} is ${diff}");
+
+  var pro = n1 * n2;
+  print("product of ${n1} and ${n2} is ${pro}");
+
+  var div = n2 / n1;
+  print("division of ${n1} and ${n2} is ${div}");
+
+  var mod = n2 % n1;
+  print("reminder of ${n1} and ${n2} is ${mod}");
+}
